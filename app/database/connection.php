b553c379c082f2401955479d0a5064791eca7cda
Kelvin Pilger <kelvinpilgeer@gmail.com>
2025-04-23 17:53:04 -0300

    Feat: Class de Connection

---
+++ app/database/connection.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Database;
+
+class Connection {
+	private static $connection = null;
+	
+	public static function openConn() {
+		if(self::$connection === null) {
+			self::$connection = new mysqli("localhost", "root", '', "db_servfacil", 3306);
+			if(self::$connection->connect_error) {
+				die("Erro na conexão: " . self::$connection->connect_error);
+			}
+		}
+		return self::$connection;
+	}
+}
+?>
